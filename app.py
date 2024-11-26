98dade1ef76662d66b34dd3731bda10540465032
daltonkdwyer <dalton.dwyer@gmail.com>
2024-11-26 15:35:59 -0500

    a

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
 # gunicorn your_application.wsgi
